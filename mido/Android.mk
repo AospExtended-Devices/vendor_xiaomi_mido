d796a30933423961d46fd58fd5b756294352abe6
thp1997 <tirthpatel49@gmail.com>
2017-07-23 14:40:51 +0000

    Revert "mido: Remove QTI Telephony blobs"
    This reverts commit e9bf7ab77ab9001c731d86b718be464afb6542e3.

--- mido/Android.mk
+++ mido/Android.mk
@@ -64,6 +64,16 @@ LOCAL_MODULE_SUFFIX := .so
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := QtiTelephonyService
+LOCAL_MODULE_OWNER := xiaomi
+LOCAL_SRC_FILES := proprietary/app/QtiTelephonyService/QtiTelephonyService.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
